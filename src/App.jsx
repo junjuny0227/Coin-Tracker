e6554eeaa6773578af9ca70eb568734e0de0a1ce
junjuny0227 <junjuny0227@gmail.com>
2024-09-06 22:40:28 +0900

    Get Coin Information

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
 import "./tailwind.css";
+import axios from "axios";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://api.coinpaprika.com/v1/tickers").then((response) => {
+      setCoins(response.data);
+      setLoading(false);
+    });
+  }, []);
+
   return (
     <div>
-      <h1>Coin Tracker</h1>
+      <h1>Coin Tracker ({coins.length})</h1>
+      {loading ? <strong>Loading...</strong> : null}
+      <select>
+        {coins.map((coin) => (
+          <option>
+            {coin.name} ({coin.symbol}): ${coin.quotes.USD.price}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
