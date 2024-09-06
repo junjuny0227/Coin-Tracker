1c2bfa4213ef19547e8ce912e7a89c605f79e8e0
junjuny0227 <junjuny0227@gmail.com>
2024-09-07 01:00:28 +0900

    simple design

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,29 @@ function App() {
   const [coins, setCoins] = useState([]);
 
   useEffect(() => {
-    axios.get("https://api.coinpaprika.com/v1/tickers").then((response) => {
-      setCoins(response.data);
-      setLoading(false);
-    });
+    setTimeout(() => {
+      axios.get("https://api.coinpaprika.com/v1/tickers").then((response) => {
+        setCoins(response.data);
+        setLoading(false);
+      });
+    }, 2000);
   }, []);
 
   return (
-    <div>
-      <h1>Coin Tracker {loading ? "" : `(${coins.length})`}</h1>
+    <div
+      className="flex flex-col justify-center items-center h-screen font-mono bg-cover "
+      style={{
+        backgroundImage:
+          "url('https://www.gi-de.com/corporate/_processed_/3/1/csm_Header_fuer_Coin_Produktseite_879ba68ace.jpg')",
+      }}
+    >
+      <h1 className="text-7xl font-black mb-5 bg-white border-4 border-black rounded-lg pl-1.5 pb-2.5">
+        Coin Tracker {loading ? "" : `(${coins.length})`}
+      </h1>
       {loading ? (
-        <strong>Loading...</strong>
+        <p className="text-2xl font-block">Loading...</p>
       ) : (
-        <select>
+        <select className="text-2xl font-block w-1/3 border-2 border-black rounded-lg outline-none">
           {coins.map((coin) => (
             <option>
               {coin.name} ({coin.symbol}): ${coin.quotes.USD.price}
